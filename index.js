18e303881b4abd11ebaba3eb87a61446c5a5dbd4
Kris Janssen Cabansag <kjs.cabansag@gmail.com>
2024-02-07 11:42:48 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your solution here!
+const cats =["Milo", "Otis", "Garfield"];
+
+// const destructivelyAppendCat = [...cats, "Ralph"];
+
+function destructivelyAppendCat(name){
+cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+}
+
+
+function appendCat(name) {
+    const newCats = cats.slice();
+    newCats.push(name);
+    return newCats;
+
+}
+
+function prependCat(name){
+    const kitten = cats.slice();
+    kitten.unshift(name);
+    return kitten;
+}
+
+function removeLastCat(name){
+    const lessCats = cats.slice();
+    lessCats.pop(name);
+    return lessCats;
+}
+
+function removeFirstCat(){
+    const fewCats = cats.slice();
+    fewCats.shift();
+    return fewCats;
+}
+// destructivelyAppendCat("Angel");
+// console.log(cats);
